856cff0df9124aae83dfb63daba4d2e28135d70b
GATI-Rus <ruskom83@gmail.com>
2024-01-12 17:53:29 +0300

    Start2

--- app/settings.py
+++ app/settings.py
@@ -39,8 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'main',
-    'app',
-]
+    'goods',
+    ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -120,6 +120,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS: list = [
+    BASE_DIR / 'static'
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
--- main/views.py
+++ main/views.py
@@ -1,15 +1,19 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
-
 def index(request):
-    context: dict = {
-        'title': 'Home',
-        'content': 'Главная страница магазина'
+    context = {
+        'title': 'Home - Главная',
+        'content': 'Магазин мебели HOME'
     }
 
     return render(request, 'main/index.html', context)
 
 def about(request):
-    return HttpResponse('About page')
+    context = {
+        'title': 'Home - О нас',
+        'content': 'О нас',
+        'text_on_page': 'Текст о том почему этот магазин такой классный, и какой хороший товар.'
+    }
+
+    return render(request, 'main/about.html', context)
