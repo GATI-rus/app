c5ffcff9a46f38001737661106f2ced06dbe1a9b
GATI-Rus <ruskom83@gmail.com>
2024-02-02 13:29:34 +0300

    Sessions - carts for anonymous user

--- users/views.py
+++ users/views.py
@@ -1,10 +1,11 @@
 from django.contrib.auth.decorators import login_required
 from django.contrib import auth, messages
 from django.http import HttpResponseRedirect
-from django.shortcuts import render, redirect
+from django.shortcuts import redirect, render
 from django.urls import reverse
+from carts.models import Cart
 
-from users.forms import UserLoginForm, UserRegistrationForm, ProfileForm
+from users.forms import ProfileForm, UserLoginForm, UserRegistrationForm
 
 
 def login(request):
@@ -14,9 +15,15 @@ def login(request):
             username = request.POST['username']
             password = request.POST['password']
             user = auth.authenticate(username=username, password=password)
+
+            session_key = request.session.session_key
+
             if user:
                 auth.login(request, user)
                 messages.success(request, f"{username}, Вы вошли в аккаунт")
+
+                if session_key:
+                    Cart.objects.filter(session_key=session_key).update(user=user)
 
                 redirect_page = request.POST.get('next', None)
                 if redirect_page and redirect_page != reverse('user:logout'):
@@ -25,20 +32,26 @@ def login(request):
                 return HttpResponseRedirect(reverse('main:index'))
     else:
         form = UserLoginForm()
-
     context = {
         'title': 'Home - Авторизация',
         'form': form
     }
     return render(request, 'users/login.html', context)
 
+
 def registration(request):
     if request.method == 'POST':
         form = UserRegistrationForm(data=request.POST)
         if form.is_valid():
             form.save()
+
+            session_key = request.session.session_key
+
             user = form.instance
             auth.login(request, user)
+
+            if session_key:
+                Cart.objects.filter(session_key=session_key).update(user=user)
             messages.success(request, f"{user.username}, Вы успешно зарегистрированы и вошли в аккаунт")
             return HttpResponseRedirect(reverse('main:index'))
     else:
@@ -61,13 +74,12 @@ def profile(request):
             return HttpResponseRedirect(reverse('user:profile'))
     else:
         form = ProfileForm(instance=request.user)
-
-
     context = {
         'title': 'Home - Кабинет',
-        'form': form,
+        'form': form
     }
     return render(request, 'users/profile.html', context)
+
 
 def users_cart(request):
     return render(request, 'users/users_cart.html')
