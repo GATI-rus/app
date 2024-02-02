551c52e351fe07ddae32ce35568714658d240805
GATI-Rus <ruskom83@gmail.com>
2024-02-02 20:00:35 +0300

    Final edits

--- users/views.py
+++ users/views.py
@@ -1,9 +1,11 @@
 from django.contrib.auth.decorators import login_required
 from django.contrib import auth, messages
+from django.db.models import Prefetch
 from django.http import HttpResponseRedirect
 from django.shortcuts import redirect, render
 from django.urls import reverse
 from carts.models import Cart
+from orders.models import Order, OrderItem
 
 from users.forms import ProfileForm, UserLoginForm, UserRegistrationForm
 
@@ -32,6 +34,7 @@ def login(request):
                 return HttpResponseRedirect(reverse('main:index'))
     else:
         form = UserLoginForm()
+
     context = {
         'title': 'Home - Авторизация',
         'form': form
@@ -63,7 +66,6 @@ def registration(request):
     }
     return render(request, 'users/registration.html', context)
 
-
 @login_required
 def profile(request):
     if request.method == 'POST':
@@ -74,9 +76,19 @@ def profile(request):
             return HttpResponseRedirect(reverse('user:profile'))
     else:
         form = ProfileForm(instance=request.user)
+
+    orders = Order.objects.filter(user=request.user).prefetch_related(
+        Prefetch(
+            "orderitem_set",
+            queryset=OrderItem.objects.select_related("product"),
+        )
+    ).order_by("-id")
+
+
     context = {
         'title': 'Home - Кабинет',
-        'form': form
+        'form': form,
+        'orders': orders,
     }
     return render(request, 'users/profile.html', context)
 
